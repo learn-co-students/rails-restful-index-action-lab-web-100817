63a2ea3fb16a8cfe0eefc8de9c391b3eda503044
Dakota Lillie <dakota.lillie@icloud.com>
2017-11-03 16:44:46 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,30 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+  
+  def show
+    
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def delete
+    
+  end
+end
